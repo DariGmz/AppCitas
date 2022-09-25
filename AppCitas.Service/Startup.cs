8ea8eb26141af73eaf83bca9d125e1482bd865ef
DarianaGomez <dariana.gomez@cceo.com.mx>
2022-09-25 17:12:10 -0500

    Adding CORS support in the API

--- AppCitas.Service/Startup.cs
+++ AppCitas.Service/Startup.cs
@@ -22,6 +22,8 @@ public class Startup
             options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
         });
         services.AddControllers();
+        services.AddCors();
+
         services.AddSwaggerGen(c =>
         {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -41,6 +43,8 @@ public class Startup
         app.UseHttpsRedirection();
 
         app.UseRouting();
+
+        app.UseCors(p => p.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
         app.UseAuthorization();
 
