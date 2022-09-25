13fbc9e82939609a086495b33e754a90eb03295c
DarianaGomez <dariana.gomez@cceo.com.mx>
2022-09-25 18:41:47 -0500

    Adding a DateTime extension to calculate age

--- AppCitas.Service/Entities/AppUser.cs
+++ AppCitas.Service/Entities/AppUser.cs
@@ -6,7 +6,7 @@ public class AppUser
     public string UserName { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
-    public DateOnly DateOfBirth { get; set; }
+    public DateTime DateOfBirth { get; set; }
     public string KnownAs { get; set; }
     public DateTime Created { get; set; } = DateTime.Now;
     public DateTime LastActive { get; set; } = DateTime.Now;
@@ -17,4 +17,9 @@ public class AppUser
     public string City { get; set; }
     public string Country { get; set; }
     public ICollection<Photo> Photos { get; set; }
+
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    }
 }
