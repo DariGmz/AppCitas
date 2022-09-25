f8148af70f6518f5665417c3584e6432a60c5bb8
DarianaGomez <dariana.gomez@cceo.com.mx>
2022-09-25 17:48:13 -0500

    Adding validation in username and password, adding a login endpoint

--- AppCitas.Service/Controllers/AccountController.cs
+++ AppCitas.Service/Controllers/AccountController.cs
@@ -38,6 +38,27 @@ public class AccountController : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+        if (user == null) return Unauthorized("Invalid username or password");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+                return Unauthorized("Invalid username or password");
+        }
+
+        return user;
+    }
+
     #region Private methods
 
     private async Task<bool> UserExists(string username)
