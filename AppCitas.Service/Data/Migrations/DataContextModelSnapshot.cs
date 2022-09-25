17c88ee8f8ca5fb89b664f8d857341dba1ed2fa7
DarianaGomez <dariana.gomez@cceo.com.mx>
2022-09-25 18:49:53 -0500

    Entity Framework relationships

--- AppCitas.Service/Data/Migrations/DataContextModelSnapshot.cs
+++ AppCitas.Service/Data/Migrations/DataContextModelSnapshot.cs
@@ -18,24 +18,95 @@ namespace AppCitas.Service.Data.Migrations
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
 
             modelBuilder.Entity("AppCitas.Service.Entities.AppUser", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
 
-                    b.Property<byte[]>("PasswordHash")
-                        .HasColumnType("BLOB");
+                b.Property<string>("City")
+                    .HasColumnType("TEXT");
 
-                    b.Property<byte[]>("PasswordSalt")
-                        .HasColumnType("BLOB");
+                b.Property<string>("Country")
+                    .HasColumnType("TEXT");
 
-                    b.Property<string>("UserName")
-                        .HasColumnType("TEXT");
+                b.Property<DateTime>("Created")
+                    .HasColumnType("TEXT");
 
-                    b.HasKey("Id");
+                b.Property<DateTime>("DateOfBirth")
+                    .HasColumnType("TEXT");
 
-                    b.ToTable("Users");
-                });
+                b.Property<string>("Gender")
+                    .HasColumnType("TEXT");
+
+                b.Property<string>("Interests")
+                    .HasColumnType("TEXT");
+
+                b.Property<string>("Introduction")
+                    .HasColumnType("TEXT");
+
+                b.Property<string>("KnownAs")
+                    .HasColumnType("TEXT");
+
+                b.Property<DateTime>("LastActive")
+                    .HasColumnType("TEXT");
+
+                b.Property<string>("LookingFor")
+                    .HasColumnType("TEXT");
+
+                b.Property<byte[]>("PasswordHash")
+                    .HasColumnType("BLOB");
+
+                b.Property<byte[]>("PasswordSalt")
+                    .HasColumnType("BLOB");
+
+                b.Property<string>("UserName")
+                    .HasColumnType("TEXT");
+
+                b.HasKey("Id");
+
+                b.ToTable("Users");
+            });
+
+            modelBuilder.Entity("AppCitas.Service.Entities.Photo", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
+
+                b.Property<int>("AppUserId")
+                    .HasColumnType("INTEGER");
+
+                b.Property<bool>("IsMain")
+                    .HasColumnType("INTEGER");
+
+                b.Property<string>("PublicId")
+                    .HasColumnType("TEXT");
+
+                b.Property<string>("Url")
+                    .HasColumnType("TEXT");
+
+                b.HasKey("Id");
+
+                b.HasIndex("AppUserId");
+
+                b.ToTable("Photos");
+            });
+
+            modelBuilder.Entity("AppCitas.Service.Entities.Photo", b =>
+            {
+                b.HasOne("AppCitas.Service.Entities.AppUser", "AppUser")
+                    .WithMany("Photos")
+                    .HasForeignKey("AppUserId")
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .IsRequired();
+
+                b.Navigation("AppUser");
+            });
+
+            modelBuilder.Entity("AppCitas.Service.Entities.AppUser", b =>
+            {
+                b.Navigation("Photos");
+            });
 #pragma warning restore 612, 618
         }
     }
